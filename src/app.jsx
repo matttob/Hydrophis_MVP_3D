fd582468781c1b98de014416899dbb9d4c66ca3b
matttob <matttoberman@ghotmail.com>
2024-01-22 09:15:05 +0000

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import './index.css'
+
+import Home from './home.jsx'
+import Layout from "./Layout.jsx"
+import About from "./about.jsx"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <Routes>
+    <Route element={<Layout />}>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+    </Route>
+  </Routes>
+</BrowserRouter>,
+)
+
+
+
