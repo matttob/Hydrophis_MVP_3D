e0af3894a962f2a200d5afb854795d3d2127f019
matttob <matttoberman@ghotmail.com>
2024-01-25 12:39:22 +0000

    add router provider

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom"
 import './index.css'
 
 import Home from './home.jsx'
-import Layout from "./layout.jsx"
+import Layout from "./Layout.jsx"
 import About from "./about.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Routes>
-    <Route element={<Layout />}>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-    </Route>
-  </Routes>
-</BrowserRouter>,
-)
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+
+<Route element={<Layout />}>
+  <Route path="/" element={<Home />}/>
+  <Route path="/about" element={<About />}/>
+</Route>
+
+))
+
+export default function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+
 
 
 
